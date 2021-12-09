1450a623722a5e7a9f086d14769802b104b953e8
leobtt <leonarbat8@gmail.com>
2021-12-09 08:37:06 -0300

    server

--- src/pages/Generos/index.js
+++ src/pages/Generos/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Generos = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/genres').then((res) => setData(res.data.data))
+  }, [])
   return (
     <div>
       <h1>Genêros</h1>
+      <pre>{JSON.stringify(data)}</pre>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Genêro</th>
+            <th scope="col">Ação</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    axios.get('/api').then((res) => {
+      setData(res.data)
+    })
+  }, [])
+
   return (
     <div>
       <h1>home</h1>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
   )
 }
