c372c71fca09766bb90a0761de260c4f3bbf26ce
leobtt <leonarbat8@gmail.com>
2021-12-15 08:36:43 -0300

    component

---
+++ src/components/ListaItens.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const listaItens = (route, { id, name }, deletar) => {
+  return (
+    <tr key={id.toString()} className="text-center align-middle">
+      <td className="mx-auto">{id}</td>
+      <td>{name}</td>
+      <td className="d-flex justify-content-around">
+        <button className="btn btn-danger" onClick={() => deletar(id)}>
+          Remover
+        </button>
+        <Link to={route + id} className="btn btn-warning">
+          Editar
+        </Link>
+      </td>
+    </tr>
+  )
+}
+
+export default listaItens
