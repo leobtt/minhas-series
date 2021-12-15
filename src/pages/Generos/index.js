44bf372875c53616e50202f2e6e9792f6755db8f
leobtt <leonarbat8@gmail.com>
2021-12-15 08:37:21 -0300

    .

--- src/pages/Generos/index.js
+++ src/pages/Generos/index.js
@@ -1,25 +1,59 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import listaItens from '../../components/ListaItens'
 
 const Generos = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    axios.get('/api/genres').then((res) => setData(res.data.data))
+    axios.get('/api/genres').then((res) => {
+      setData(res.data.data)
+    })
+    return () => {}
   }, [])
+
+  if (data.length === 0) {
+    return (
+      <div className="container">
+        <h1>Genêros</h1>
+        <div className="alert alert-warning" role="alert">
+          Você não possui genêros listadas.
+        </div>
+        <Link className="p-2 mb-2 btn btn-primary" to="/generos/novo">
+          Novo Genêro
+        </Link>
+      </div>
+    )
+  }
+
+  const deletar = (id) => {
+    axios.delete('/api/genres/' + id).then((res) => {
+      const filtrado = data.filter((item) => item.id !== id)
+      setData(filtrado)
+    })
+  }
+
   return (
-    <div>
-      <h1>Genêros</h1>
-      <pre>{JSON.stringify(data)}</pre>
-      <table class="table table-dark">
+    <div className="container">
+      <div className="mt-4 mb-4 px-4 d-flex justify-content-between">
+        <h1>Genêros</h1>
+        <Link className="p-2 mb-2 btn btn-primary" to="/generos/novo">
+          Novo Genêro
+        </Link>
+      </div>
+      <table className="table table-dark table-bordered">
         <thead>
-          <tr>
+          <tr className="text-center">
             <th scope="col">Id</th>
             <th scope="col">Genêro</th>
             <th scope="col">Ação</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {data.map((item) => listaItens('/generos/', item, deletar))}
+        </tbody>
       </table>
     </div>
   )
--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -20,25 +20,29 @@ const Header = () => {
   return (
     <div>
       <Navbar className="" color="light" light expand="md">
-        <NavbarBrand className="container-xl" tag={Link} to="/">
-          Minhas séries
-        </NavbarBrand>
-        <NavbarToggler onClick={acionar} />
-        <Collapse isOpen={open} navbar>
-          <Nav className="ms-auto" navbar>
-            <NavItem>
-              <NavLink tag={Link} to="/">
-                Home
-              </NavLink>
-            </NavItem>
+        <div className="container d-md-flex ">
+          <div className="d-flex justify-content-between align-items-center">
+            <NavbarBrand className="p-2 me-auto" tag={Link} to="/">
+              Minhas séries
+            </NavbarBrand>
+            <NavbarToggler className="" onClick={acionar} />
+          </div>
+          <Collapse className="" isOpen={open} navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem className="p-3 text-center">
+                <NavLink tag={Link} to="/series">
+                  Séries
+                </NavLink>
+              </NavItem>
 
-            <NavItem>
-              <NavLink tag={Link} to="/generos">
-                Genêros
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
+              <NavItem className="p-3 text-center">
+                <NavLink tag={Link} to="/generos">
+                  Genêros
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </div>
       </Navbar>
       <Outlet />
     </div>
